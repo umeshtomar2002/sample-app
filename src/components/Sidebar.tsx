e5e82d66a86ce3b33bc5dfe4cb3041fb0c81444e
umeshtomar2002 <umeshtomar2002@gmail.com>
2023-03-25 11:24:07 +0530

    commiting all

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,63 +1,39 @@
-import React,{useEffect} from "react";
-import Helmet from "react-helmet";
-import MenuBack from '../assets/images/menu_back.svg';
-import LogoImage from  '../assets/images/Logo.svg';
-import Myprofile from'../assets/images/my_profile.svg';
-import ScanSvg from'../assets/images/scan.svg';
-import ReportSvg from'../assets/images/report.svg';
-import LogoutSvg from'../assets/images/logout.svg';
-import { useNavigate } from "react-router-dom";
-import jQuery from 'jquery';
+import { Button, Drawer } from "antd";
+import React, { useState } from "react";
 
+export default function Sidebar(){
 
-export default function sidebar(){
-    let navigate = useNavigate();
-    
-    function performanceTestPage() {
-        navigate('/performTest'); 
-    }
+    const [open, setOpen] = useState(false);
+  
 
-    function viewReportPage() {
-        navigate('/viewReport'); 
-    }
-
-    function addUserPage() {
-        navigate('/addUser');
-    }
-
-    useEffect(() => {
-            <Helmet>            
-                <script type = "text/javascript" src="../src/assets/js/jquery.min.js"/>
-                <script type = "text/javascript" src="../src/assets/js/browser.min.js"/>
-                <script type = "text/javascript" src="../src/assets/js/breakpoints.min.js"/>
-                <script type = "text/javascript" src="../src/assets/js/util.js"/>
-                <script type = "text/javascript" src="../src/assets/js/main.js"/>
-           </Helmet>
-      });
+    const showDrawer = () => {
+      setOpen(true);
+    };
+  
+    const onClose = () => {
+      setOpen(false);
+    };
+  
 
     return(
         <>
-            
-         <div id="sidebar">
-                <div className="inner">
-                    
-                    <section id="logo" className="alt align-center">
-                        <img src={LogoImage} alt="Drivn Fintech" width="40%" />
-                    </section>
-                    
-                    
-                    <nav id="menu">
-                        <ul>
-                            <li><a href="#" onClick={()=>addUserPage()}><img src={Myprofile} alt=""/> My Profile</a></li>
-                            <li><a href="#" onClick={()=>performanceTestPage()}><img src={ScanSvg} alt=""/>Measure Now</a></li>
-                            <li><a href="#" onClick={()=>viewReportPage()}><img src={ReportSvg} alt=""/> View Reports</a></li>
-                            <li><a href="#"><img src={LogoutSvg} alt=""/> Logout</a></li>	
-                        </ul>
-                    </nav>
-
-                    <div className="bottom_menu"><img src={MenuBack}/></div>
-                </div>
-            </div>
+             <Button className="primary" onClick={showDrawer}>
+                    Open
+                </Button>
+            <Drawer                
+                title="Basic Drawer"
+                placement="left"
+                closable={false}
+                onClose={onClose}
+                open={open}
+                key="left"         
+            >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                
+            </Drawer>
         </>
     )
-}
+
+ }
