027d3b9e105914c5706670c65981dc3ab1d192a4
vishalchavanvicky <vishalchavanvicky@gmail.com>
2023-05-21 22:25:27 +0530

    Loginbanner and speedometer svg change

--- src/components/CurrentReport.tsx
+++ src/components/CurrentReport.tsx
@@ -26,16 +26,16 @@ export default function currentReport() {
     const [healthList, setHealthList] = useState([])
     const [spinner, setSpinner] = useState(false)
     let meterTransformStyle = { transform: "rotate(-110deg)" };
- 
+
     function binahPage() {
         navigate('/binah');
     }
 
-    const getWhatsappLink = () => {       
-        if(healthList.length>0){
-            let message=""
-            for (let i=0;i<healthList.length;i++) {
-                message = message+getMessage(healthList[i]);
+    const getWhatsappLink = () => {
+        if (healthList.length > 0) {
+            let message = ""
+            for (let i = 0; i < healthList.length; i++) {
+                message = message + getMessage(healthList[i]);
             }
             return `https://api.whatsapp.com/send?text=${message}`
         }
@@ -45,7 +45,7 @@ export default function currentReport() {
         let time = (location.state.page == 'binah') ? new Date().toLocaleString() : new Date(data.createdAt).toLocaleString()
         delete data.updatedAt;
         delete data.familyId;
-        return `--- ${time} ---%0a`+JSON.stringify(data).replace(/,/g, '%0a').replace(/["'}{]/g,"")+"%0a%0a"
+        return `--- ${time} ---%0a` + JSON.stringify(data).replace(/,/g, '%0a').replace(/["'}{]/g, "") + "%0a%0a"
     }
 
     function CurrentDatetime() {
@@ -125,16 +125,16 @@ export default function currentReport() {
             wellnessLevel: binahData.wellnessLevel != null && binahData.wellnessLevel != undefined && binahData.wellnessLevel.value != null ? binahData.wellnessLevel.value : 'N/A',
         }
         useEffect(() => {
-          setSpinner(true);
-          setHealthList([value]);
-          saveHealthData(value)
-            .then((res) => {
-              setSpinner(false);
-            })
-            .catch((err) => {
-              setSpinner(false);
-              console.log("error => " + err);
-            });
+            setSpinner(true);
+            setHealthList([value]);
+            saveHealthData(value)
+                .then((res) => {
+                    setSpinner(false);
+                })
+                .catch((err) => {
+                    setSpinner(false);
+                    console.log("error => " + err);
+                });
         }, []);
     }
 
@@ -163,13 +163,13 @@ export default function currentReport() {
     }
     let meterResult = "N/A"
     let indicator = "low"
-    
+
     if (healthList.length > 0 && healthList[0].wellnessLevel && healthList[0].wellnessLevel != "N/A") {
-        let indicatorSign = ["low","medium","high"]
+        let indicatorSign = ["low", "medium", "high"]
         // indicator = indicatorSign[(Math.round(healthList[0].wellnessIndex / 3.33))-1]     
-        indicator = indicatorSign[(healthList[0].wellnessLevel)-1] 
+        indicator = indicatorSign[(healthList[0].wellnessLevel) - 1]
         // console.log("wellnessLevel:::", healthList[0].wellnessLevel)
-    
+
     }
     if (healthList.length > 0 && healthList[0].wellnessIndex && healthList[0].wellnessIndex != "N/A") {
         meterResult = healthList[0]?.wellnessIndex + "/10"
@@ -178,14 +178,14 @@ export default function currentReport() {
         // console.log("wellnessIndex:::", healthList[0].wellnessIndex)
     }
 
-    
-    const getBloodPressure = (d) =>{
-        if(d.systolicBp && d.diastolicBp && d.systolicB!="N/A" && d.diastolicBp!="N/A"){
-            return (d.systolicBp+"/"+d.diastolicBp)
+
+    const getBloodPressure = (d) => {
+        if (d.systolicBp && d.diastolicBp && d.systolicB != "N/A" && d.diastolicBp != "N/A") {
+            return (d.systolicBp + "/" + d.diastolicBp)
         }
         return "N/A"
     }
-    
+
 
     return (
         <>
@@ -199,18 +199,19 @@ export default function currentReport() {
 
                                 <div className="score">
                                     <div className="meter">
-                                        <svg width="327" height="250" id="meter" viewBox="0 0 327 317" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <rect x="14" y="9" width="301" height="202" fill="#db0000" />
-                                            <path d="M239.772 49.4556C244.43 43.0045 253.464 41.4512 259.516 46.6171C270.965 56.3903 280.907 67.765 289.025 80.3783C293.396 87.1699 290.473 96.0346 283.259 99.665L211.127 135.96C197.546 142.794 183.924 126.811 192.823 114.485L239.772 49.4556Z" fill="#EE952C" />
-                                            <path d="M25.8782 210.701C18.3813 213.426 10.0168 209.605 7.95331 201.9C0.679942 174.742 1.07306 146.032 9.23397 118.929C18.7845 87.2122 38.461 59.3983 65.3263 39.6394C92.1916 19.8805 124.805 9.23643 158.297 9.29607C186.857 9.34693 214.75 17.1783 238.961 31.7942C245.884 35.9735 247.169 45.232 242.28 51.6735L197.01 111.32C192.2 117.657 183.205 118.709 175.744 115.947C170.142 113.874 164.176 112.787 158.117 112.776C147.22 112.757 136.61 116.219 127.869 122.648C119.129 129.076 112.727 138.125 109.62 148.444C107.932 154.051 107.265 159.869 107.612 165.634C108.096 173.685 104.452 182.148 96.8709 184.903L25.8782 210.701Z" fill="url(#paint0_linear_498_1159)" />
-                                            <path fillRule="evenodd" clipRule="evenodd" d="M0 0H326.285V221.242H0V0ZM34.6883 206.881C27.2032 209.764 18.7216 206.046 16.606 198.308C11.7205 180.441 10.3127 161.752 12.5111 143.268C15.2828 119.964 23.7028 97.6888 37.0397 78.3781C50.3766 59.0674 68.2269 43.3052 89.0398 32.461C109.853 21.6167 132.999 16.0184 156.467 16.1524C179.935 16.2863 203.016 22.1486 223.703 33.2297C244.391 44.3109 262.06 60.2759 275.176 79.7376C288.291 99.1993 296.457 121.569 298.962 144.904C300.949 163.412 299.328 182.082 294.239 199.893C292.035 207.606 283.512 211.228 276.06 208.259L239.45 193.676C231.999 190.707 228.516 182.258 230.009 174.377C231.388 167.098 231.695 159.63 230.899 152.212C229.583 139.959 225.296 128.213 218.409 117.994C211.522 107.774 202.244 99.3914 191.381 93.5728C180.518 87.7542 168.399 84.676 156.076 84.6057C143.753 84.5353 131.599 87.475 120.671 93.1692C109.742 98.8634 100.369 107.14 93.3662 117.28C86.3631 127.42 81.9419 139.116 80.4865 151.353C79.6053 158.762 79.8275 166.233 81.123 173.527C82.5256 181.425 78.9471 189.834 71.462 192.717L34.6883 206.881ZM96.5323 176.472C97.1844 178.411 99.3523 179.328 101.25 178.565C103.148 177.802 104.056 175.648 103.418 173.704C100.905 166.041 100.05 157.916 100.928 149.87C101.939 140.596 105.223 131.713 110.488 124.011C115.752 116.308 122.835 110.022 131.109 105.711C139.383 101.399 148.593 99.1946 157.922 99.2925C167.252 99.3904 176.413 101.788 184.595 106.272C192.777 110.756 199.726 117.189 204.828 125C209.93 132.812 213.026 141.761 213.843 151.055C214.552 159.117 213.526 167.223 210.853 174.832C210.175 176.761 211.037 178.934 212.919 179.737C214.8 180.539 216.987 179.668 217.679 177.743C220.822 169.009 222.038 159.682 221.222 150.407C220.298 139.899 216.798 129.781 211.03 120.95C205.262 112.118 197.405 104.846 188.155 99.7761C178.905 94.7064 168.548 91.9962 158 91.8855C147.452 91.7748 137.04 94.2671 127.686 99.1416C118.332 104.016 110.324 111.122 104.372 119.831C98.4202 128.539 94.7078 138.581 93.5639 149.067C92.554 158.324 93.5731 167.674 96.5323 176.472Z" fill="white" />  
+                                        <svg viewBox="10.803 2.322 487.13 351.337"  id="meter" width="327" height="180" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="21.501" y="25.69" width="445.016" height="312.142" fill="#db0000"></rect>
+                                            <path d="M 52.278 119.742 C 59.165 109.772 214.444 1.72 223.392 9.703 C 240.319 24.805 399.305 78.651 411.307 98.142 C 417.769 108.637 413.448 122.336 402.782 127.945 L 296.138 206.107 C 276.059 216.667 158.938 218.777 172.095 199.73 L 52.278 119.742 Z" fill="#EE952C"></path>
+                                            <path d="M 140.524 36.175 C 129.44 31.964 117.072 37.868 114.022 49.775 C 103.269 91.741 103.85 136.105 115.916 177.986 C 130.035 226.997 128.822 283.968 164.617 261.382 C 174.853 254.924 298.921 165.302 298.921 165.302 C 291.81 155.51 256.692 80.297 245.482 76.039 L 140.524 36.175 Z" fill="url(#paint0_linear_498_1159)" transform="matrix(-1, 0, 0, -1, 571.195199, 382.217932)"></path>
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M 10.803 11.783 L 497.933 2.322 L 493.202 353.659 L 10.803 353.659 L 10.803 11.783 Z M 62.088 331.467 C 51.022 335.922 38.481 330.177 35.354 318.22 C 28.131 290.611 26.049 261.731 29.3 233.169 C 33.397 197.158 45.845 162.738 65.564 132.897 C 85.282 103.057 111.672 78.7 142.444 61.944 C 173.216 45.187 207.437 36.535 242.133 36.742 C 276.83 36.949 310.954 46.009 341.539 63.132 C 372.125 80.255 398.248 104.925 417.64 134.999 C 437.03 165.071 449.103 199.638 452.806 235.697 C 455.744 264.297 453.347 293.146 445.824 320.669 C 442.565 332.588 429.964 338.185 418.947 333.597 L 364.82 311.062 C 353.804 306.474 348.655 293.418 350.862 281.24 C 352.901 269.992 353.355 258.452 352.178 246.99 C 350.232 228.056 343.894 209.905 333.712 194.114 C 323.53 178.322 309.813 165.368 293.752 156.377 C 277.692 147.385 259.774 142.629 241.555 142.521 C 223.336 142.411 205.367 146.954 189.21 155.753 C 173.052 164.552 159.195 177.342 148.841 193.011 C 138.487 208.68 131.949 226.753 129.798 245.662 C 128.496 257.111 128.824 268.656 130.74 279.927 C 132.813 292.131 127.523 305.125 116.457 309.58 L 62.088 331.467 Z M 153.522 284.478 C 154.486 287.474 157.691 288.891 160.497 287.712 C 163.303 286.533 164.646 283.204 163.702 280.2 C 159.987 268.359 158.723 255.804 160.021 243.371 C 161.516 229.04 166.371 215.313 174.155 203.412 C 181.938 191.509 192.41 181.795 204.642 175.134 C 216.875 168.471 230.492 165.065 244.284 165.215 C 258.078 165.366 271.623 169.072 283.719 176.001 C 295.816 182.929 306.09 192.87 313.633 204.94 C 321.176 217.012 325.753 230.84 326.961 245.202 C 328.009 257.66 326.493 270.185 322.541 281.943 C 321.538 284.924 322.813 288.282 325.595 289.523 C 328.376 290.762 331.61 289.416 332.633 286.442 C 337.279 272.945 339.077 258.533 337.871 244.2 C 336.505 227.963 331.33 212.328 322.802 198.682 C 314.275 185.034 302.658 173.797 288.983 165.963 C 275.307 158.128 259.994 153.94 244.4 153.769 C 228.805 153.599 213.411 157.45 199.582 164.983 C 185.752 172.514 173.913 183.495 165.113 196.953 C 156.313 210.409 150.823 225.926 149.132 242.13 C 147.64 256.434 149.147 270.882 153.522 284.478 Z" fill="white"></path>
                                             <defs>
-                                                <linearGradient id="paint0_linear_498_1159" x1="364.347" y1="300.909" x2="-48.8075" y2="255.096" gradientUnits="userSpaceOnUse">
-                                                    <stop offset="0.0677083" stopColor="#1663D6" />
-                                                    <stop offset="0.786458" stopColor="#01C738" />
+                                                <linearGradient id="paint0_linear_498_1159" x1="364.347" y1="300.909" x2="-48.8075" y2="255.096" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1.47846, 0, 0, -1.545257, 102.263957, 361.762299)">
+                                                    <stop offset="0.0677" stopColor="#1663D6" />
+                                                    <stop offset="0.7865" stopColor="#01C738" />
                                                 </linearGradient>
                                             </defs>
-                                        </svg><svg width="30" height="144" id="needle" style={meterTransformStyle} viewBox="0 0 30 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        </svg>
+                                        <svg width="30" height="144" id="needle" style={meterTransformStyle} viewBox="0 0 30 144" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <circle cx="15.2089" cy="128.608" r="10.9086" fill="white" stroke="#01C738" strokeWidth="7" />
                                             <path fillRule="evenodd" clipRule="evenodd" d="M17.3486 0.920656L25.3277 127.679L25.3166 127.679C25.322 127.833 25.3239 127.988 25.322 128.144C25.2587 133.535 20.8377 137.853 15.4474 137.79C10.0571 137.726 5.73875 133.305 5.80203 127.915C5.80386 127.759 5.80934 127.604 5.81838 127.45L5.80766 127.45L17.3486 0.920656ZM19.2238 134.487C22.4036 132.316 23.2212 127.978 21.0498 124.798C18.8784 121.618 14.5404 120.801 11.3606 122.972C8.18076 125.144 7.36323 129.482 9.53459 132.661C11.706 135.841 16.044 136.659 19.2238 134.487Z" fill="url(#paint0_linear_498_1180)" />
                                             <defs>
@@ -220,7 +221,7 @@ export default function currentReport() {
                                                 </linearGradient>
                                             </defs>
                                         </svg>
-                                        <h3>{meterResult}</h3>
+                                        <h3 className="meterResult">{meterResult}</h3>
                                     </div>
 
                                     <div className="result-score-text"><h3>Your Welness Score is {indicator}.</h3>
@@ -270,7 +271,7 @@ export default function currentReport() {
                                     })}
 
 
-                                <p id="parameter_disclaimer">** Marked parameters are presently under clinical research.</p>
+                                    <p id="parameter_disclaimer">** Marked parameters are presently under clinical research.</p>
                                 </ul>
                                 <p id="reportButtons" className="clear text-center">
                                     <a href={getWhatsappLink()} data-action="share/whatsapp/share" target="_blank"
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,7 +5,7 @@ import { checkLogin } from "./Client";
 import { useNavigate } from "react-router-dom";
 import LoginSvg from '../assets/images/Logo.svg';
 // import LoginBanner from '../assets/images/loginBanner.jpeg';
-import LoginBanner from '../assets/images/loginBanner_converted.svg';
+import LoginBanner from '../assets/images/loginFinalBanner.svg';
 // const LoginBanner =  require("../assets/images/loginBanner.jpeg") as string;
 
 
